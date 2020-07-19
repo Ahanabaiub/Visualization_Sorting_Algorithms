a68dd1c919562b9c9a95d22d4e7fa36ef7f155a6
Ahanab Shakil <shieam47@gmail.com>
2020-07-20 00:00:54 +0600

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace sortingAlgorithmVisualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(65)))), ((int)(((byte)(92)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.ForeColor = System.Drawing.Color.Turquoise;
+            this.label3.Name = "label3";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4"),
+            resources.GetString("comboBox1.Items5"),
+            resources.GetString("comboBox1.Items6")});
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            resources.GetString("listBox1.Items"),
+            resources.GetString("listBox1.Items1"),
+            resources.GetString("listBox1.Items2")});
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.Color.Turquoise;
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.Turquoise;
+            this.label1.Name = "label1";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(41)))));
+            this.panel3.Controls.Add(this.button3);
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.Name = "panel3";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseCompatibleTextRendering = true;
+            this.button3.UseMnemonic = false;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.panel3);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sortingAlgorithmVisualizer
+{
+    public partial class Form1 : Form
+    {
+
+        static int elements=100;
+        static float gap = 8.016F;
+        static float lw =  gap-1;
+        static string algo ;
+        static int delay=10;
+        static int h,w;
+        static bool reset = false;
+        bool start = false;
+        static bool stop = false;
+        int[] arr = new int[100000 ];
+
+        Graphics g;
+        Pen myPen = new Pen(Color.Turquoise,lw);
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            generateElements();
+            h = panel2.Height;
+            w = panel2.Width;
+
+        }
+
+        private void swap(int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+
+
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+           
+            if(reset==true)
+            {
+                for (int i = 0; i < elements; i++)
+                {
+                    g.DrawLine(new Pen(Color.Turquoise, lw), (float)i *(gap), h, (float)i *(gap), h - arr[i]);
+                  //MessageBox.Show((float)i * (gap)+"");
+                }
+                reset = false;
+            }
+
+           if(start)
+            {
+                start = false;
+
+                // Bubble sort
+                if (algo == "Bubble Sort")
+                {
+                    for(int i=0; i<elements; i++)
+                    {
+                        for(int j=0; j<elements-i-1; j++)
+                        {
+                            g.DrawLine(new Pen(Color.Black, lw), (float)j * gap, h, (float)j * gap, 0);
+                            g.DrawLine(new Pen(Color.Black, lw), (float)(j + 1) * gap, h, (float)(j + 1) * gap, 0);
+                            g.DrawLine(new Pen(Color.FromArgb(227, 18, 178), lw), (float)j * gap, h, (float)j * gap, h - arr[j]);
+                            System.Threading.Thread.Sleep(delay);
+                           
+                            g.DrawLine(new Pen(Color.FromArgb(227, 18, 178), lw), (float)(j + 1) * gap, h, (float)(j + 1) * gap, h - arr[j + 1]);
+                            System.Threading.Thread.Sleep(delay);
+
+                            if (arr[j]>arr[j+1])
+                            {
+                               
+                                g.DrawLine(new Pen(Color.Black, lw), (float)j *gap, h, (float)j *gap, 0);
+                                g.DrawLine(new Pen(Color.Black, lw), (float)(j + 1) * gap, h, (float)(j + 1) * gap, 0);
+
+                                g.DrawLine(new Pen(Color.Turquoise, lw), (float)j *gap, h, (float)j *gap, h- arr[j+1]);
+                                g.DrawLine(new Pen(Color.Turquoise, lw), (float)(j + 1)*gap, h, (float)(j + 1)*gap, h - arr[j]);
+                              
+
+                                swap(j, j + 1);
+                            }
+                            else
+                            {
+                              
+                                g.DrawLine(new Pen(Color.Turquoise, lw), (float)j * gap, h, (float)j * gap, h - arr[j]);
+                      
+                                g.DrawLine(new Pen(Color.Turquoise, lw), (float)(j + 1) * gap, h, (float)(j + 1) * gap, h - arr[j + 1]);
+                               
+                            }
+                        }
+                    }
+
+                }
+                else if(algo=="Insertion Sort")
+                {
+                    for(int i=1; i<elements; i++)
+                    {
+                        int key = arr[i];
+                        g.DrawLine(new Pen(Color.FromArgb(242, 22, 22), lw), i * gap, h, i * gap, h - arr[i]);
+                        System.Threading.Thread.Sleep(delay);
+                        int j = i - 1;
+
+                        while (j>=0 && arr[j]>key)
+                        {
+                            g.DrawLine(new Pen(Color.FromArgb(242, 22, 22), lw), (j+1) * gap, h, (j+1) * gap, h-arr[j]);
+                            System.Threading.Thread.Sleep(delay);
+                            g.DrawLine(new Pen(Color.Black, lw), (j+1) * gap, h, (j+1) * gap, 0);
+             
+                            g.DrawLine(new Pen(Color.FromArgb(3, 252, 136), lw), (j+1) * gap, h, (j+1) * gap, h - arr[j]);
+                           System.Threading.Thread.Sleep(delay);
+                            arr[j + 1] = arr[j];
+                            j--;
+                        }
+                        arr[j+1] = key;
+                        g.DrawLine(new Pen(Color.Black, lw), (j+1) * gap, h, (j+1) * gap, 0);
+                        g.DrawLine(new Pen(Color.FromArgb(3, 252, 136), lw), (j+1) * gap, h, (j+1) * gap, h - key);
+                        System.Threading.Thread.Sleep(delay);
+                    }
+                }
+                else if(algo=="Selection Sort")
+                {
+                    for(int i=0; i<elements; i++)
+                    {
+                        int min_ind = i;
+                        g.DrawLine(new Pen(Color.FromArgb(242, 22, 22), lw), (float)i * gap, h, (float)i * gap, h - arr[i]);
+
+
+                        for (int j=i+1; j<elements; j++)
+                        {
+                            g.DrawLine(new Pen(Color.FromArgb(3, 252, 136), lw), (float)j * gap, h, (float)j * gap, h - arr[j]);
+                            System.Threading.Thread.Sleep(delay);
+                            g.DrawLine(new Pen(Color.Black, lw), (float)j * gap, h, (float)j * gap, 0);
+                            g.DrawLine(new Pen(Color.Turquoise, lw), (float)j * gap, h, (float)j * gap, h - arr[j]);
+                            System.Threading.Thread.Sleep(delay);
+
+                            if (arr[j]<arr[min_ind])
+                            {
+                                min_ind = j;                  
+                            }
+                        }
+
+                        g.DrawLine(new Pen(Color.Black, lw), (float)i * gap, h, (float)i * gap, 0);
+                        g.DrawLine(new Pen(Color.FromArgb(225, 235, 52), lw), (float)i * gap, h, (float)i * gap, h - arr[min_ind]);
+                        System.Threading.Thread.Sleep(delay);
+
+
+                        swap(i, min_ind);
+
+                    }
+                }
+            }
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            algo = listBox1.Items[listBox1.SelectedIndex].ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+       
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            try
+            {
+                elements = Convert.ToInt32(textBox1.Text);
+
+                if (elements < 1)
+                    elements = 1;
+                gap = w / elements;
+                lw = gap - 1;
+            }
+            catch(FormatException ex)
+            {
+                MessageBox.Show("Enter number of elements !!");
+               
+            }
+
+          
+        }
+
+
+        private void generateElements()
+        {
+            h = panel2.Height;
+           
+
+            for (int i=0; i<elements; i++)
+            {
+                arr[i] = rand.Next(2, h);
+               
+            }
+
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generateElements();
+            reset = true;
+            panel2.Invalidate();
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            start = true;
+            reset = true;
+            panel2.Invalidate();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.Text==null)
+            {
+                delay = 10;
+                comboBox1.Text = "10";
+            }
+            else
+            {
+                delay = int.Parse(comboBox1.GetItemText(comboBox1.SelectedItem));
+            }
+        }
+
+
+    }
+}
